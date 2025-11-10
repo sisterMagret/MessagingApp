3aaeeb87efceddbd575272fe078a704a3a2afcbe
sisterMagret <wistler4u@gmail.com>
2025-11-10 12:09:53 +0100

    api: add MessagesController endpoints (send, inbox, mark-read) and JWT claim parsing

--- src/Api/Controllers/MessagesController.cs
+++ src/Api/Controllers/MessagesController.cs
@@ -1,35 +1,57 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
+using Core.Dtos;
 using Core.Interfaces;
-using Api.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-[Authorize]
-public class MessagesController : ControllerBase
+namespace Api.Controllers
 {
-    private readonly IMessageService _messages;
-
-    public MessagesController(IMessageService messages)
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class MessagesController : ControllerBase
     {
-        _messages = messages;
-    }
+        private readonly IMessageService _messages;
 
-    [HttpPost]
-    public async Task<IActionResult> Send(MessageCreateDto dto)
-    {
-        var senderId = int.Parse(User.FindFirst("nameid")!.Value);
-        var result = await _messages.SendAsync(senderId, dto.ReceiverId, dto.Content);
-        return Ok(result);
-    }
+        public MessagesController(IMessageService messages)
+        {
+            _messages = messages;
+        }
 
-    [HttpGet("inbox")]
-    public async Task<IActionResult> Inbox()
-    {
-        var userId = int.Parse(User.FindFirst("nameid")!.Value);
-        var msgs = await _messages.GetInboxAsync(userId);
-        return Ok(msgs);
+        // POST api/messages
+        [HttpPost]
+        public async Task<IActionResult> Send([FromBody] MessageCreateRequest request)
+        {
+            // parse sender id from JWT claims
+            var nameId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            if (!int.TryParse(nameId, out var senderId))
+                return Unauthorized();
+
+            var dto = await _messages.SendAsync(senderId, request);
+            return Ok(dto);
+        }
+
+        // GET api/messages/inbox?page=1&pageSize=50
+        [HttpGet("inbox")]
+        public async Task<IActionResult> Inbox([FromQuery] int page = 1, [FromQuery] int pageSize = 50)
+        {
+            var nameId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            if (!int.TryParse(nameId, out var userId))
+                return Unauthorized();
+
+            var messages = await _messages.GetInboxAsync(userId, page, pageSize);
+            return Ok(messages);
+        }
+
+        // POST api/messages/{id}/read
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkRead([FromRoute] int id)
+        {
+            var nameId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            if (!int.TryParse(nameId, out var userId))
+                return Unauthorized();
+
+            await _messages.MarkAsReadAsync(userId, id);
+            return NoContent();
+        }
     }
 }
